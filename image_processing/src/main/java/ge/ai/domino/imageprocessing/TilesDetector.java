9eabcf8b3716307fce47a32fc5f03847ea79ed57
Iosebi Gobronidze <ioseb.gobronidze@azry.com>
2018-06-28 16:06:59 +0400

    added imageprocessing module

---
+++ image_processing/src/main/java/ge/ai/domino/imageprocessing/TilesDetector.java
@@ -0,0 +1,12 @@
+package ge.ai.domino.imageprocessing;
+
+import ge.ai.domino.domain.game.Tile;
+
+import java.util.List;
+
+public class TilesDetector {
+
+	public static List<Tile> getTiles() {
+		return null;
+	}
+}

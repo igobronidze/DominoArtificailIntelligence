20482121527083237fde3f6cf5017275c093c7d6
Iosebi Gobronidze <ioseb.gobronidze@azry.com>
2018-11-23 09:43:16 +0400

    fixed images memory leak

--- console/ui/src/main/java/ge/ai/domino/console/ui/util/ImageFactory.java
+++ console/ui/src/main/java/ge/ai/domino/console/ui/util/ImageFactory.java
@@ -2,9 +2,18 @@ package ge.ai.domino.console.ui.util;
 
 import javafx.scene.image.Image;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImageFactory {
 
+    private static Map<String, Image> cachedImages = new HashMap<>();
+
     public static Image getImage(String name) {
-        return new Image("static/images/" + name);
+        if (!cachedImages.containsKey(name)) {
+            Image image = new Image("static/images/" + name);
+            cachedImages.put(name, image);
+        }
+        return cachedImages.get(name);
     }
 }
